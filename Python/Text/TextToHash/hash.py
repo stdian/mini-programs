c8d86eb99ec1fd5dec07cfd979126bbc138372d5
Dmitriy Stepanov <stdian@mail.ru>
2018-07-16 18:48:25 +0800

    TextToHash - Added sha256 algorithm

--- Python/Text/TextToHash/hash.py
+++ Python/Text/TextToHash/hash.py
@@ -12,10 +12,14 @@ def main():
 	print('Choose hash type:')
 	print()
 	print('1. MD5')
+	print('2. SHA256')
 	hash_type = int(input())
 
 	if hash_type == 1:
 		result = hashlib.md5(text.encode("utf")).hexdigest()
+		print('Result: ' + result)
+	elif hash_type == 2:
+		result = hashlib.sha256(text.encode("utf")).hexdigest()
 		print('Result: ' + result)
 
 
