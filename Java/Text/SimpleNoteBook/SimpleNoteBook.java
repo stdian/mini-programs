871accc702522f71a7157fd051632634b09709eb
Dmitriy Stepanov <stdian@mail.ru>
2018-09-11 17:08:11 +0800

    SimpleNoteBook - Added registration and login. Added notes encrypt.

--- Java/Text/SimpleNoteBook/SimpleNoteBook.java
+++ Java/Text/SimpleNoteBook/SimpleNoteBook.java
@@ -1,16 +1,19 @@
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Scanner;
 
 public class SimpleNoteBook {
-	File file = new File("notes.txt");
+	static File file;
+	static File users = new File("users.bin");
+	static String session = "";
 	ArrayList<String> notes = new ArrayList<>();
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 		Scanner in = new Scanner(System.in);
 
 		String userInput;
@@ -18,25 +21,43 @@ public class SimpleNoteBook {
 		System.out.println("Welcome to notebook\n\n");
 
 		while (true) {
-			System.out.println("1. Show notes");
-			System.out.println("2. Add note");
-			System.out.println("3. Remove notes");
-			System.out.println("4. Exit");
+			if (session.equals("")) {
 
-			userInput = in.nextLine().trim();
+				if(! users.exists()){
+					users.createNewFile();
+				}
 
-			switch (userInput) {
-				case "1":
-					new SimpleNoteBook().printNotes();
-					break;
-				case "2":
-					new SimpleNoteBook().addNotes();
-					break;
-				case "3":
-					new SimpleNoteBook().clearNotes();
-					break;
-				case "4":
+				System.out.println("1. Login\n2. Register\n3. Exit");
+				userInput = in.nextLine().trim();
+
+				if (userInput.equals("1")) {
+					new SimpleNoteBook().login();
+				} else if (userInput.equals("2")) {
+					new SimpleNoteBook().register();
+				} else if (userInput.equals("3")) {
 					System.exit(0);
+				}
+			} else {
+				System.out.println("1. Show notes");
+				System.out.println("2. Add note");
+				System.out.println("3. Remove notes");
+				System.out.println("4. Exit");
+				file = new File(session + ".bin");
+				userInput = in.nextLine().trim();
+
+				switch (userInput) {
+					case "1":
+						new SimpleNoteBook().printNotes();
+						break;
+					case "2":
+						new SimpleNoteBook().addNotes();
+						break;
+					case "3":
+						new SimpleNoteBook().clearNotes();
+						break;
+					case "4":
+						System.exit(0);
+				}
 			}
 		}
 
@@ -45,12 +66,17 @@ public class SimpleNoteBook {
 	public void printNotes() throws IOException {
 		readNotes();
 
-		System.out.println("Notes:\n");
+		if (! notes.isEmpty()) {
 
-		for (String note : notes) {
-			System.out.println(note);
+			System.out.println("Notes:\n");
+
+			for (String note : notes) {
+				byte[] decodedText = Base64.getDecoder().decode(note);
+				System.out.println(new String(decodedText));
+			}
+		} else {
+			System.out.println("Your notes are empty!");
 		}
-
 		System.out.println();
 	}
 
@@ -76,7 +102,9 @@ public class SimpleNoteBook {
 		System.out.print("Text: ");
 		String text = in.nextLine();
 
-		notes.add(text);
+		byte[] encodedText = Base64.getEncoder().encode(text.getBytes());
+
+		notes.add(new String(encodedText));
 
 		PrintWriter out = new PrintWriter(file.getAbsoluteFile());
 
@@ -100,5 +128,86 @@ public class SimpleNoteBook {
 		}
 
 		notes.clear();
+	}
+
+	public void register() throws NoSuchAlgorithmException, UnsupportedEncodingException, FileNotFoundException {
+
+		ArrayList<String> data = new ArrayList<>();
+
+		Scanner sc = new Scanner(users);
+
+		while (sc.hasNextLine()) {
+			data.add(sc.nextLine());
+		}
+
+		PrintWriter out = new PrintWriter(users.getAbsoluteFile());
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.reset();
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Input login: ");
+		String login = in.nextLine().trim();
+		System.out.print("Input password: ");
+		String password = in.nextLine().trim();
+
+		for(String user : data) {
+			String[] loginPassword = user.split(":");
+			if (login.equals(loginPassword[0])) {
+				System.out.println("This login already used!");
+				return;
+			}
+		}
+
+		if (! login.equals("") && ! password.equals("")) {
+			m.update(password.getBytes("utf-8"));
+			String hashedPassword = new BigInteger(1,m.digest()).toString(16);
+
+			while(hashedPassword.length() < 32 ){
+				hashedPassword = "0" + hashedPassword;
+			}
+
+			data.add(login + ":" + hashedPassword);
+
+			try {
+				for (String user : data) {
+					out.println(user);
+				}
+			} finally {
+				out.close();
+			}
+		}
+	}
+
+	public void login() throws NoSuchAlgorithmException, UnsupportedEncodingException, FileNotFoundException {
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.reset();
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Input login: ");
+		String login = in.nextLine().trim();
+		System.out.print("Input password: ");
+		String password = in.nextLine().trim();
+
+		if (! login.equals("") && ! password.equals("")) {
+			m.update(password.getBytes("utf-8"));
+			String hashedPassword = new BigInteger(1,m.digest()).toString(16);
+
+			while(hashedPassword.length() < 32 ){
+				hashedPassword = "0" + hashedPassword;
+			}
+
+			Scanner sc = new Scanner(users);
+
+			while (sc.hasNextLine()) {
+				String data = sc.nextLine();
+				String[] loginPassword = data.split(":");
+				if (login.equals(loginPassword[0]) && hashedPassword.equals(loginPassword[1])) {
+					session = login;
+					System.out.println("\nWelcome, " + session);
+					return;
+				}
+			}
+			System.out.println("Wrong login or password!\n");
+		}
 	}
 }
